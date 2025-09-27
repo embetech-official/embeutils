8b332318795feab8b88b1568fe6d664b0b380dfc
Jan Macheta <2372096+jmacheta@users.noreply.github.com>
2025-09-28 00:21:42 +0200

    hide status messages from install steps

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -7,7 +7,9 @@ set(PROJECT_COPYRIGHT "Embetech sp. z o.o.")
 set_target_properties(embeutils PROPERTIES EXPORT_NAME utils)
 set(CMAKES_EXPORT_DIR cmake)
 
-write_basic_package_version_file(embeutils-config-version.cmake COMPATIBILITY SameMajorVersion ARCH_INDEPENDENT)
+write_basic_package_version_file(
+  embeutils-config-version.cmake COMPATIBILITY SameMajorVersion ARCH_INDEPENDENT
+)
 
 configure_package_config_file(
   ${PROJECT_SOURCE_DIR}/cmake/embeutils-config.cmake.in
@@ -17,14 +19,11 @@ configure_package_config_file(
 install(TARGETS embeutils EXPORT embeutils_targets)
 
 set(INCLUDE_COMMON_DIR ${PROJECT_SOURCE_DIR}/src/include)
-file(GLOB HEADERS_TO_CONFIGURE RELATIVE ${INCLUDE_COMMON_DIR}
-     "${INCLUDE_COMMON_DIR}/embetech/*.h"
-)
+file(GLOB HEADERS_TO_CONFIGURE RELATIVE ${INCLUDE_COMMON_DIR} "${INCLUDE_COMMON_DIR}/embetech/*.h")
 
-message(STATUS "Configuring headers: ${HEADERS_TO_CONFIGURE}")
+message(VERBOSE "Configuring headers: ${HEADERS_TO_CONFIGURE}")
 
 install(DIRECTORY src/include/embetech DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-
 
 # Invoke the code below during the install phase - this will replace the placeholders in the headers
 # with the actual values
@@ -36,7 +35,7 @@ install(
 
   foreach(header ${HEADERS_TO_CONFIGURE})
     set(exported_header \${CMAKE_INSTALL_PREFIX}/include/\${header})
-    message(STATUS \"Configuring doxygen header: \${exported_header}\")
+    message(VERBOSE \"Configuring doxygen header: \${exported_header}\")
     configure_file(\${exported_header} \${exported_header})
   endforeach()
   "
@@ -47,8 +46,6 @@ install(FILES ${CMAKE_CURRENT_BINARY_DIR}/embeutils-config.cmake
         DESTINATION ${CMAKES_EXPORT_DIR}
 )
 
-install(FILES LICENSE DESTINATION ${CMAKE_INSTALL_PREFIX}
-        RENAME LICENSE.txt)
-
+install(FILES LICENSE DESTINATION ${CMAKE_INSTALL_PREFIX} RENAME LICENSE.txt)
 
 install(EXPORT embeutils_targets NAMESPACE embetech:: DESTINATION ${CMAKES_EXPORT_DIR})
